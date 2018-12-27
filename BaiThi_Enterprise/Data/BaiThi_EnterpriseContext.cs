4279d28cd75538567569cc654350cdeb265293e8
Tinh77 <tinhgiang9898@gmail.com>
2018-12-27 11:21:08 +0700

    Nop-Bai

---
+++ BaiThi_Enterprise/Data/BaiThi_EnterpriseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiThi_Enterprise.Models
+{
+    public class BaiThi_EnterpriseContext : DbContext
+    {
+        public BaiThi_EnterpriseContext (DbContextOptions<BaiThi_EnterpriseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BaiThi_Enterprise.Models.Employee> Employee { get; set; }
+    }
+}
